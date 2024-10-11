4b81d749b9b5b7d6ec518caa88cdba15ab218900
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-10-11 11:20:28 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,7 +17,7 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) == 'M');
+    assert(size(38) == 'S');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
